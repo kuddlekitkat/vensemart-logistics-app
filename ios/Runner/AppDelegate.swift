4c352d4fa525d7acd4478d0896644113ea999985
Weki Work labs <ukonuzoidx@gmail.com>
2024-05-08 07:43:03 +0100

    onesignal work

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
 import GoogleMaps
+import OneSignal
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,7 +9,12 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("ADD YOUR GOOGLE MAP API KEY")
+    OneSignal.setLogLevel(.LL_VERBOSE, visualLevel: .LL_NONE)
+  
+  // OneSignal initialization
+    OneSignal.initWithLaunchOptions(launchOptions)
+    OneSignal.setAppId("580dc8b3-a23b-4ef4-9ec9-fa1fd78c83bb")
+    GMSServices.provideAPIKey("AIzaSyAg5nyQAzPupKmzUJKyM-Rp09gAmGJqzyc")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
