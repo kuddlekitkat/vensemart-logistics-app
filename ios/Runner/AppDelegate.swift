d508231c0efbca136904503461ae288df22ec76e
Weki Work labs <ukonuzoidx@gmail.com>
2024-05-10 09:08:28 +0100

    Omo update choke

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,7 @@
 import UIKit
 import Flutter
 import GoogleMaps
-import OneSignal
+// import OneSignal
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,11 +9,11 @@ import OneSignal
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    OneSignal.setLogLevel(.LL_VERBOSE, visualLevel: .LL_NONE)
+  //   OneSignal.setLogLevel(.LL_VERBOSE, visualLevel: .LL_NONE)
   
-  // OneSignal initialization
-    OneSignal.initWithLaunchOptions(launchOptions)
-    OneSignal.setAppId("580dc8b3-a23b-4ef4-9ec9-fa1fd78c83bb")
+  // // OneSignal initialization
+  //   OneSignal.initWithLaunchOptions(launchOptions)
+  //   OneSignal.setAppId("580dc8b3-a23b-4ef4-9ec9-fa1fd78c83bb")
     GMSServices.provideAPIKey("AIzaSyAg5nyQAzPupKmzUJKyM-Rp09gAmGJqzyc")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
